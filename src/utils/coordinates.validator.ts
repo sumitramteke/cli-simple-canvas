6aeca36ce14174a73db3c519271661c46726793e
Sumit Ramteke <sumitvramteke@outlook.com>
2022-01-24 20:00:04 +0530

    correction in validation, outline print, canvas array

--- src/utils/coordinates.validator.ts
+++ src/utils/coordinates.validator.ts
@@ -1,7 +1,9 @@
 const validateCoordinates = (layout: any[][], [x1, y1, x2, y2]: number []) => {
-  let isValid = Array.isArray(layout[y1]) && layout[y1][x1] !== undefined;
+  const X = layout[0].length;
+  const Y = layout.length;
+  let isValid = x1 > 0 && x1 <= X && y1 > 0 && y1 <= Y;
   if (isValid && x2 !== undefined && y2 !== undefined) {
-    isValid = Array.isArray(layout[y2]) && layout[y2][x2] !== undefined;
+    isValid = x2 > 0 && x2 <= X && y2 > 0 && y2 <= Y;
   }
   return isValid;
 }
--- src/utils/outline.paint.ts
+++ src/utils/outline.paint.ts
@@ -1,7 +1,11 @@
 const paintOutline = (outline: any[][]): any[][] => {
+  const topBottomBorders = Array.from({ length: outline[0].length + 2 })
+                                .map(item => '-').join('');
+  console.log(topBottomBorders);
   for (const row of outline) {
-    console.log(row.join(''));
+    console.log(`|${row.join('')}|`);
   }
+  console.log(topBottomBorders);
   return outline;
 }
 
